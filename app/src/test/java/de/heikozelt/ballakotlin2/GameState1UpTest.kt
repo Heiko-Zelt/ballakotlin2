8bd38a1f5b19d404087bcc708f12b37d87ed4b3f
Heiko-Zelt <hz@heikozelt.de>
2021-12-26 18:07:32 +0100

    added unit tests for GameState1Up

--- app/src/test/java/de/heikozelt/ballakotlin2/GameState1UpTest.kt
+++ app/src/test/java/de/heikozelt/ballakotlin2/GameState1UpTest.kt
@@ -2,16 +2,117 @@ package de.heikozelt.ballakotlin2
 
 import de.heikozelt.ballakotlin2.model.GameState
 import de.heikozelt.ballakotlin2.model.GameState1Up
+import org.junit.Assert.*
 import org.junit.Test
-import org.junit.Assert.assertEquals
 
+// Todo: many more sequences could be testet
 class GameState1UpTest {
 
     @Test
     fun constructor_simple() {
         val gs = GameState(3, 2, 3)
-        val listener = GameStateListenerMock()
-        val gs1Up = GameState1Up(gs, listener)
+        val gs1Up = GameState1Up(gs)
         assertEquals(gs, gs1Up.getGameState())
+        assertFalse(gs1Up.isUp())
+    }
+
+    @Test
+    fun register() {
+        val gs = GameState(3, 2, 3)
+        val gs1Up = GameState1Up(gs)
+        val listener = GameStateListenerMock()
+        gs1Up.registerGameStateListener(listener)
+
+        assertFalse(gs1Up.isUp())
+        assertTrue(listener.observationsLog.isEmpty())
+    }
+
+    @Test
+    fun click_on_tube_0_once() {
+        val gs = GameState(3, 2, 3)
+        gs.tubes[0].addBall(1)
+        val gs1Up = GameState1Up(gs)
+        val listener = GameStateListenerMock()
+        gs1Up.registerGameStateListener(listener)
+
+        gs1Up.tubeClicked(0)
+
+        assertTrue(gs1Up.isUp())
+        assertEquals(0, gs1Up.getUpCol())
+        assertEquals(1, listener.observationsLog.size)
+        assertEquals("liftBall(col=0, row=0, color=1)", listener.observationsLog.get(0))
+    }
+
+    @Test
+    fun click_on_tube_0_twice() {
+        val gs = GameState(3, 2, 3)
+        gs.tubes[0].addBall(1)
+        val gs1Up = GameState1Up(gs)
+        val listener = GameStateListenerMock()
+        gs1Up.registerGameStateListener(listener)
+
+        gs1Up.tubeClicked(0)
+        gs1Up.tubeClicked(0)
+
+        assertFalse(gs1Up.isUp())
+        assertEquals(2, listener.observationsLog.size)
+        assertEquals("liftBall(col=0, row=0, color=1)", listener.observationsLog.get(0))
+        assertEquals("dropBall(col=0, row=0, color=1)", listener.observationsLog.get(1))
+    }
+
+    @Test
+    fun click_on_tube_0_and_1() {
+        val gs = GameState(3, 2, 3)
+        gs.tubes[0].addBall(1)
+        val gs1Up = GameState1Up(gs)
+        val listener = GameStateListenerMock()
+        gs1Up.registerGameStateListener(listener)
+
+        gs1Up.tubeClicked(0)
+        gs1Up.tubeClicked(1)
+
+        assertFalse(gs1Up.isUp())
+        assertEquals(3, listener.observationsLog.size)
+        assertEquals("liftBall(col=0, row=0, color=1)", listener.observationsLog.get(0))
+        assertEquals("holeBall(fromCol=0, toCol=1, toRow=0, color=1)", listener.observationsLog.get(1))
+        assertEquals("enableResetAndUndo(enabled=true)", listener.observationsLog.get(2))
+    }
+
+    @Test
+    fun click_on_undo() {
+        val gs = GameState(3, 2, 3)
+        gs.tubes[0].addBall(1)
+        val gs1Up = GameState1Up(gs)
+        val listener = GameStateListenerMock()
+        gs1Up.registerGameStateListener(listener)
+
+        gs1Up.tubeClicked(0)
+        gs1Up.tubeClicked(1)
+        gs1Up.actionUndo()
+
+        assertFalse(gs1Up.isUp())
+        assertEquals(5, listener.observationsLog.size)
+        assertEquals("liftBall(col=0, row=0, color=1)", listener.observationsLog.get(0))
+        assertEquals("holeBall(fromCol=0, toCol=1, toRow=0, color=1)", listener.observationsLog.get(1))
+        assertEquals("enableResetAndUndo(enabled=true)", listener.observationsLog.get(2))
+        assertEquals("enableResetAndUndo(enabled=false)", listener.observationsLog.get(3))
+        assertEquals("liftAndHoleBall(fromCol=1, toCol=0, fromRow=0, toRow=0, color=1)", listener.observationsLog.get(4))
+    }
+
+    @Test
+    fun click_on_new_game() {
+        val gs = GameState(3, 2, 3)
+        val gs1Up = GameState1Up(gs)
+        val listener = GameStateListenerMock()
+        gs1Up.registerGameStateListener(listener)
+
+        gs1Up.actionNewGame()
+
+        assertFalse(gs1Up.isUp())
+        assertEquals(4, listener.observationsLog.size)
+        assertEquals("enableResetAndUndo(enabled=false)", listener.observationsLog.get(0))
+        assertEquals("enableCheat(enabled=true)", listener.observationsLog.get(1))
+        assertEquals("redraw()", listener.observationsLog.get(2))
+        assertEquals("newGameToast()", listener.observationsLog.get(3))
     }
 }
--- app/src/test/java/de/heikozelt/ballakotlin2/GameStateListenerMock.kt
+++ app/src/test/java/de/heikozelt/ballakotlin2/GameStateListenerMock.kt
@@ -1,37 +1,45 @@
 package de.heikozelt.ballakotlin2
 
 import de.heikozelt.ballakotlin2.model.GameStateListenerInterface
+import de.heikozelt.ballakotlin2.model.Move
 
 class GameStateListenerMock: GameStateListenerInterface {
+
+    var observationsLog = mutableListOf<String>()
+
     override fun redraw() {
-        TODO("Not yet implemented")
+        observationsLog.add("redraw()")
     }
 
     override fun liftBall(col: Int, row: Int, color: Int) {
-        TODO("Not yet implemented")
+        observationsLog.add("liftBall(col=${col}, row=${row}, color=${color})")
     }
 
     override fun dropBall(col: Int, row: Int, color: Int) {
-        TODO("Not yet implemented")
+        observationsLog.add("dropBall(col=${col}, row=${row}, color=${color})")
     }
 
     override fun holeBall(fromCol: Int, toCol: Int, toRow: Int, color: Int) {
-        TODO("Not yet implemented")
+        observationsLog.add("holeBall(fromCol=${fromCol}, toCol=${toCol}, toRow=${toRow}, color=${color})")
     }
 
     override fun liftAndHoleBall(fromCol: Int, toCol: Int, fromRow: Int, toRow: Int, color: Int) {
-        TODO("Not yet implemented")
+        observationsLog.add("liftAndHoleBall(fromCol=${fromCol}, toCol=${toCol}, fromRow=${fromRow}, toRow=${toRow}, color=${color})")
     }
 
-    override fun enableUndo() {
-        TODO("Not yet implemented")
+    override fun enableUndoAndReset(enabled: Boolean) {
+        observationsLog.add("enableResetAndUndo(enabled=${enabled})")
     }
 
-    override fun disableUndo() {
-        TODO("Not yet implemented")
+    override fun enableCheat(enabled: Boolean) {
+        observationsLog.add("enableCheat(enabled=${enabled})")
     }
 
     override fun puzzleSolved() {
-        TODO("Not yet implemented")
+        observationsLog.add("puzzleSolved()")
+    }
+
+    override fun newGameToast() {
+        observationsLog.add("newGameToast()")
     }
 }
